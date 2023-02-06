20328f922f174fea11800a083a2fc2e438a77303
Aniel1922 <anielshadday@gmail.com>
2023-02-06 17:17:09 -0300

    First commit desafio 1

---
+++ src/Program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+
+        // TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro irá juntar nos meses da entrada
+        int valorTotal = mesada * entrada;//Variável criada para receber a multiplicação
+
+        System.out.println(valorTotal);//saída para divulgar o valor desejado
+    }
+}
